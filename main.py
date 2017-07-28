a2d0f6deb892384f5f3cec25749d16ec69b4c75b
Zhongjie Shen <jasonshen96115@gmail.com>
2017-07-28 15:36:03 +0800

    Done

--- main.py
+++ main.py
@@ -4,32 +4,46 @@
 
 import urllib.request
 import xlwt
+import time
 
 print (">> Welcome")
 # test_url = https://www.google.com/finance/getprices?i=360&p=10d&f=d,o,h,l,c,v&df=cpct&q=AAPL
 
-url = input('>> please input your url: ')
+url = "default"
+urls = []
 
 print(">> program runing")
 
-# retrieving content from the websites (which are txt files)
-respond = urllib.request.urlopen(url)
-txt = respond.readlines()
+while url != "":
+	url = input(">> please input the url you want to use: ")
+	if url != "":
+		urls.append(url)
 
-# adding lines to excel file 
 wb = xlwt.Workbook()
-ws = wb.add_sheet('A Test Sheet')
 style0 = xlwt.easyxf('font: name Times New Roman',
     num_format_str='#,##0.00')
 
-count = 0;
+# time starts counting here
+print(">> running, please wait.")
+start_time = time.time()
 
-for i in txt:
-		ws.write(count, 0, i.decode("utf8"), style0)
+Tab = 0
+for i in urls: # get url
+	# retrieving content from the websites (which are txt files)
+	respond = urllib.request.urlopen(i)
+	lines = respond.readlines() #whole txt
+
+	ws = wb.add_sheet(str(Tab))
+	count = 0
+	for line in lines:
+		ws.write(count, 0, line.decode("utf8"), style0)
 		count += 1
+
+	Tab += 1
 
 # finishing up
 wb.save('result.xls')
-print(">> Done. result.xls saved. Please check program directory")
 respond.close()
+print(">> Done. result.xls saved. Please check program directory.")
+print(">> Total time spent: %s s." % (time.time() - start_time))
 

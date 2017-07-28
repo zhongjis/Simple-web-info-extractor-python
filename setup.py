b831820081f93fd5b1182a11f5e6d62f7e65cd37
Zhongjie Shen <jasonshen96115@gmail.com>
2017-07-28 11:02:15 +0800

    basic functionality finished

--- setup.py
+++ setup.py
@@ -3,18 +3,29 @@
 # warning: this only works for txt file
 
 import urllib.request
+import xlwt
+
+
 
 url = "https://www.google.com/finance/getprices?i=360&p=10d&f=d,o,h,l,c,v&df=cpct&q=AAPL"
 
+# retrieving content from the websites (which are txt files)
 respond = urllib.request.urlopen(url)
 txt = respond.readlines()
 
-count = 0
+# adding lines to excel file 
+wb = xlwt.Workbook()
+ws = wb.add_sheet('A Test Sheet')
+style0 = xlwt.easyxf('font: name Times New Roman',
+    num_format_str='#,##0.00')
+
+count = 0;
 
 for i in txt:
-	if count < 20:
-		print(i.decode("utf8"))
+		ws.write(count, 0, i.decode("utf8"), style0)
 		count += 1
+
+wb.save('test.xls')
 
 respond.close()
 

3946b7815ff746c8f4bda3bc4c34fb2144105a57
Zhongjie Shen <jasonshen96115@gmail.com>
2017-08-03 15:42:59 +0800

    Detail tweaks

--- function.py
+++ function.py
@@ -1,49 +1,51 @@
 # function.py
 import urllib.request
-import time
 import xlwt
 
-class db:
-	urls = []
-	url = "default"
 
-	def ask_for_urls(self):
-		while self.url != "":
-			self.url = input(">>> please input the url you want to use, or press enter to stop: ")
-			if self.url != "":
-				self.urls.append(self.url)
+class db:
+    urls = []
+    url = "default"
+
+    def ask_for_urls(self):
+        while self.url != "":
+            self.url = input(
+                """>>> Please input the url you want to import, """ +
+                """or just enter to quit: """)
+            if self.url != "":
+                self.urls.append(self.url)
+
 
 class workbook:
-	wb = xlwt.Workbook()
+    wb = xlwt.Workbook()
 
-	# add an intro page showing the adding history incase user need it
-	def add_intro(self, urls, style):
-		tab = 0
-		count = 0
-		ws = self.wb.add_sheet("Intro")
+    # add an intro page showing the adding history incase user need it
+    def add_intro(self, urls, style):
+        tab = 0
+        count = 0
+        ws = self.wb.add_sheet("Intro")
 
-		for url in urls:
-			ws.write(count, 0, str(tab), style)
-			ws.write(count, 1, url, style)
-			tab += 1
-			count += 1
+        for url in urls:
+            ws.write(count, 0, str(tab), style)
+            ws.write(count, 1, url, style)
+            tab += 1
+            count += 1
 
-	# add pages for each url
-	def add_sheets(self, urls, style):
-		tab = 0
+    # add pages for each url
+    def add_sheets(self, urls, style):
+        tab = 0
 
-		for url in urls:
-			respond = urllib.request.urlopen(url)
-			lines = respond.readlines()
+        for url in urls:
+            respond = urllib.request.urlopen(url)
+            lines = respond.readlines()
 
-			count = 0
-			ws = self.wb.add_sheet(str(tab))
-			for line in lines:
-				ws.write(count, 0, line.decode("utf8"), style)
-				count += 1
-			tab += 1
-		respond.close()
+            count = 0
+            ws = self.wb.add_sheet(str(tab))
+            for line in lines:
+                ws.write(count, 0, line.decode("utf8"), style)
+                count += 1
+            tab += 1
+        respond.close()
 
-	def save(self, file_name):
-		self.wb.save(file_name)
-
+    def save(self, file_name):
+        self.wb.save(file_name)
--- main.py
+++ main.py
@@ -3,35 +3,44 @@
 # also the UIs will be set up here too
 # warning: this program only support simple txt file on url
 
-import urllib.request
 import time
 import xlwt
 import function
 
-# test_url = https://www.google.com/finance/getprices?i=360&p=10d&f=d,o,h,l,c,v&df=cpct&q=AAPL
+# test_url = https://www.google.com/finance/
+# getprices?i=360&p=10d&f=d,o,h,l,c,v&df=cpct&q=AAPL
 
-print (">> Welcome")
+print (">>> Welcome")
 
-# data base init###########################################################
+# data base init
 db = function.db()
 db.ask_for_urls()
 
-# workbook init############################################################
-result_doc = function.workbook()
-style0 = xlwt.easyxf('font: name Times New Roman',
-    num_format_str='#,##0.00') # default style
+if db.urls == []:
+    print(">>> Quiting...")
+else:
+    # workbook init
+    result_doc = function.workbook()
+    style0 = xlwt.easyxf(
+        "font: name Times New Roman",
+        num_format_str="#,##0.00")  # default style
 
-print(">>> running, please wait.")
-# time starts counting here###############################################
-start_time = time.time()
-result_doc.add_intro(db.urls, style0)
-result_doc.add_sheets(db.urls, style0)
+    print(">>> running, please wait.")
+    # time starts counting here
+    start_time = time.time()
+    result_doc.add_intro(db.urls, style0)
+    result_doc.add_sheets(db.urls, style0)
 
-# finishing up: file name editing && saving#################################
-current_time = time.localtime();
-file_name = str(current_time.tm_year) + "_" + str(current_time.tm_mon) + "_"  + str(current_time.tm_mday) + "_" + str(current_time.tm_hour) + "_"  + str(current_time.tm_min) + "_"  + str(current_time.tm_sec) + ".xls"
+    # finishing up: file name editing && saving
+    current_time = time.localtime()
+    file_name = str(current_time.tm_year) + "_"
+    + str(current_time.tm_mon) + "_"
+    + str(current_time.tm_mday) + "_"
+    + str(current_time.tm_hour) + "_"
+    + str(current_time.tm_min) + "_"
+    + str(current_time.tm_sec) + ".xls"
 
-result_doc.save(file_name)
-print(">>> Done. Results are saved in %s. Please check program directory." % (file_name))
-print(">>> Total time spent: %ss." % (time.time() - start_time))
-
+    result_doc.save(file_name)
+    print(""">>> Done. Results are saved in %s. """ +
+          """Please check program directory.""" % (file_name))
+    print(">>> Total time spent: %ss." % (time.time() - start_time))
